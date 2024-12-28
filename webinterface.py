e193e430b9ddfb01c8a7ff741018a8225e2b6672
Carolina Cretu <150701412+CarolinaCretu@users.noreply.github.com>
2024-12-29 00:17:17 +0200

    update

--- webinterface.py
+++ webinterface.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__,static_folder='static')
 
 @app.route('/')
-def index():
+@app.route('/home')
+def home():
   return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+   file = request.files['file']
 
 if __name__ == "__main__":
    app.run(debug= True)
