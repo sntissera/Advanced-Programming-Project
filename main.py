dec22a5301437e03c1edba1a56de04ec205401b9
Carolina Cretu <150701412+CarolinaCretu@users.noreply.github.com>
2024-12-29 17:21:16 +0200

    update form

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__,static_folder='static')
+app.config['SECRET_KEY'] = 'secret'
+
+# for  files uploaded/  DNA from form 
+class InputForm(FlaskForm):
+   dna = StringField("")
+   submit= SubmitField("Submit")
+
+@app.route('/upload')
+def upload():
+   file = request.files['file']
+   
+
+
+@app.route('/', methods=["GET","POST"])
+def home():
+  dna = None
+  form = InputForm()
+  return render_template('index.html', dna=dna, form=form)
+
+if __name__ == "__main__":
+   app.run(debug= True)
