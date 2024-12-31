a4789faccd4a1b9a251542eb313329a5709828ac
Carolina Cretu <150701412+CarolinaCretu@users.noreply.github.com>
2024-12-31 21:33:24 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import FileField, StringField, SubmitField
 
 app = Flask(__name__,static_folder='static')
 app.config['SECRET_KEY'] = 'secret'
@@ -8,12 +8,8 @@ app.config['SECRET_KEY'] = 'secret'
 # for  files uploaded/  DNA from form 
 class InputForm(FlaskForm):
    dna = StringField("")
+   file = FileField("File")
    submit= SubmitField("Submit")
-
-@app.route('/upload')
-def upload():
-   file = request.files['file']
-   
 
 
 @app.route('/', methods=["GET","POST"])
@@ -21,6 +17,6 @@ def home():
   dna = None
   form = InputForm()
   return render_template('index.html', dna=dna, form=form)
-
+   
 if __name__ == "__main__":
    app.run(debug= True)
